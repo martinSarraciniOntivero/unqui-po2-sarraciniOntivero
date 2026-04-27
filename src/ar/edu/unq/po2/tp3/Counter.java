90d182346c239370136fed422cd3ddbf61bdfb81
martinSarraciniOntivero <martinsarraciniontivero@gmail.com>
2026-04-27 16:07:58 -0300

    punto 1 hecho, falta test

--- src/ar/edu/unq/po2/tp3/Counter.java
+++ src/ar/edu/unq/po2/tp3/Counter.java
@@ -1,5 +1,53 @@
 package ar.edu.unq.po2.tp3;
 
-public class Counter {
+import java.util.ArrayList; 
 
+public class Counter {
+	private ArrayList<Integer>numeros;
+	
+	public Counter() {
+		numeros = new ArrayList<Integer>();
+	}
+	public void agregarNumero(int numero) {
+		numeros.add(numero);
+	}
+	
+	public int contarPares() {
+		int contador = 0;
+		for(int numero: numeros) {
+			if(numero %2 ==0) {
+				contador += 1;
+			}
+		}
+		return contador;
+	}
+	
+	public int contarImpares() {
+		int contador = 0;
+		for(int numero: numeros) {
+			if(numero %2 !=0) {
+				contador += 1;
+			}
+		}
+		return contador;
+	}
+	
+	public int cantMultiplosDe(int x) {
+		int contador =0;
+		for(int numero: numeros) {
+			if(numero %x == 0) {
+				contador += 1;
+			}
+		}
+		return contador; 
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- test/testeoPunto1.java
+++
@@ -1,4 +0,0 @@
-
-public class testeoPunto1 {
-
-}
